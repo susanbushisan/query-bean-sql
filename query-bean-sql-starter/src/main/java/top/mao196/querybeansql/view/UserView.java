761047bc58601590670493177ad153e75f369195
susanbushisan <linkmao196@163.com>
2025-08-01 17:29:48 +0800

    feat(query-bean-sql-starter): 添加用户信息视图并创建示例表
    - 新增 UserView 类，用于展示用户信息- 在 resources/ddl/demo.sql 中创建示例用户表和插入数据
    - 删除 StoreInfoView 类，清理不再需要的视图

---
+++ query-bean-sql-starter/src/main/java/top/mao196/querybeansql/view/UserView.java
@@ -0,0 +1,21 @@
+package top.mao196.querybeansql.view;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import top.mao196.querybeansql.annotation.ViewExposed;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Data
+@ViewExposed(sql = "select id, name, age, birth, balance, create_time, create_by from user" ,name = "user", desc = "用户信息")
+public class UserView {
+    private Long id;
+    private String name;
+    private Integer age;
+    private LocalDate birth;
+    private Double balance;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+    private String createBy;
+}
